5e0694f702b8d148dc38d4d678c09b9a7b848cb7
Ronny Krammer <ronny.krammer@gwvs.de>
2025-11-30 10:19:22 +0100

    Switcher Style

--- components/ThemeSwitcher/ThemeSwitcher.tsx
+++ components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -9,7 +9,7 @@ function ThemeSwitcher() {
   const buttonClass = (isActive: boolean) => `rounded-full p-1.5 transition-colors cursor-pointer ${isActive ? 'bg-gray-700 text-gray-100' : 'bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-gray-100'}`;
 
   return (
-    <div className="flex gap-1.5">
+    <div className="flex w-fit gap-0.5 rounded-4xl border border-gray-700 p-1">
       <button onClick={() => setTheme('light')} className={buttonClass(theme === 'light')} aria-label="Light Mode" title="Light Mode">
         <SunIcon className="h-4 w-4" />
       </button>
